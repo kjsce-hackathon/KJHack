e37170485b39c66009a92005a49f9082a557dd0d
CCAtAlvis <chandakchinmay@gmail.com>
2017-10-06 20:30:49 +0530

    another web commit
    modified fr.py now it sends image url to firebase and sets it to /url/img
    save.php now recevies and saves the image directly
    no need to rename the received file
    also the python script sends the image file directly to the server

--- Web/fr.py
+++ Web/fr.py
@@ -1,9 +1,17 @@
 import requests
 
+file = "test.txt"
+
 url = "http://chinmayhomeauto.000webhostapp.com/save.php"
-#files = {'file': open('report.xls', 'rb')}
-files = {"img": "hello"}
+files = {'img': open(file, 'rb')}
+#files = {"img": "hello"}
 
 r = requests.post(url, files=files)
+
+print r.text
+
+url = "https://us-central1-auto-d6b09.cloudfunctions.net/storeImgURL"
+data = {'img': "http://chinmayhomeauto.000webhostapp.com/uploads/" + file}
+r = requests.post(url, data=data)
 
 print r.text
--- Web/post.php
+++ Web/post.php
@@ -1,32 +1,48 @@
 <?php
 
-if ( isset($_FILE) && !empty($_FILE["img"]) )
+// if ( isset($_FILE) && !empty($_FILE["img"]) )
+// {
+// 	try
+// 	{	
+// 		$img = $_FILE["img"];
+
+
+// 		$read = fopen("imgNo.txt", "r");
+// 		$imgNo = (int) fgets( $read );
+// 		fclose( $read );
+
+// 		$imgNo++;
+
+// 		$write = fopen("image$imgNo.jpeg", "w");
+// 		fwrite( $write, $img);
+// 		fclose( $write );
+
+// 		$read = fopen("imgNo.txt", "w");
+// 		fwrite( $read, $imgNo);
+// 		fclose( $read );
+
+// 		echo "success";
+// 	}
+// 	catch ( Exception $e )
+// 	{
+// 		die("error");
+// 	}
+// }
+
+
+// In PHP versions earlier than 4.1.0, $HTTP_POST_FILES should be used instead
+// of $_FILES.
+
+$uploaddir = '/storage/ssd1/528/3166528/public_html/uploads/';
+$uploadfile = $uploaddir . basename($_FILES['img']['name']);
+
+if ( move_uploaded_file($_FILES['img']['tmp_name'], $uploadfile) )
 {
-	try
-	{	
-		$img = $_FILE["img"];
-
-
-		$read = fopen("imgNo.txt", "r");
-		$imgNo = (int) fgets( $read );
-		fclose( $read );
-
-		$imgNo++;
-
-		$write = fopen("image$imgNo.jpeg", "w");
-		fwrite( $write, $img);
-		fclose( $write );
-
-		$read = fopen("imgNo.txt", "w");
-		fwrite( $read, $imgNo);
-		fclose( $read );
-
-		echo "success";
-	}
-	catch ( Exception $e )
-	{
-		die("error");
-	}
+    echo "success";
+} else {
+    echo "Possible file upload attack!\n";
 }
+
+
 
 ?>
